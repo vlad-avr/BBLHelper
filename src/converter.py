2ebf518eb17de6206344a8604f9e527fa08bcb21
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2025-05-04 14:55:10 +0300

    looking for sample data

--- src/converter.py
+++ src/converter.py
@@ -2,10 +2,11 @@ import os
 import subprocess
 
 def convert_bbl_to_csv(bbl_file):
-    """Converts .bbl file to .csv using blackbox_decode."""
+    """Converts .bbl file to .csv using blackbox_decode in WSL."""
     csv_file = bbl_file.replace(".bbl", ".csv")
     try:
-        command = f"blackbox_decode {bbl_file} > {csv_file}"
+        # Use WSL to run the Linux binary
+        command = f"wsl ./mnt/d/Унік/Диплом/blackbox-tools/obj/blackbox_decode /mnt/d/{bbl_file.replace(':', '').replace('\\', '/')} > /mnt/d/{csv_file.replace(':', '').replace('\\', '/')}"
         subprocess.run(command, shell=True, check=True)
 
         if os.path.exists(csv_file):
